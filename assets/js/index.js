fc213c3ae60a19d138fc6b2d040c55c38acaf577
Thomas Delattre <delattre.thomas@cegetel.net>
2022-02-19 10:52:48 +0100

    responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,36 +22,38 @@ mybutton.addEventListener('click', topFunction)
 
 
 
- function typeEffect(element, speed) {
-    var text = element.innerHTML;
-	element.innerHTML = "";
-	
-	var i = 0;
-	var timer = setInterval(function() {
-    if (i < text.length) {
-      element.append(text.charAt(i));
-      i++;
-    } else {
-      clearInterval(timer);
-    }
-  }, speed);
-}
+
+  //// EFFET TYPING sur le text de présentation ////
+  
+//   function typeEffect(element, speed) {
+//     var text = element.innerHTML;
+//     element.innerHTML = "";
+    
+//     var i = 0;
+//     var timer = setInterval(function() {
+//       if (i < text.length) {
+//         element.append(text.charAt(i));
+//         i++;
+//       } else {
+//         clearInterval(timer);
+//       }
+//   }, speed);
+// }
+// // application
+// var speed = 15;
+// //var h1 = document.querySelector('h1');
+// var p = document.querySelector('p');
+// var delay = p.innerHTML.length * speed + speed;
+
+// // type affect to header
+// //typeEffect(h1, speed);
 
 
-// application
-var speed = 15;
-//var h1 = document.querySelector('h1');
-var p = document.querySelector('p');
-var delay = p.innerHTML.length * speed + speed;
+// // type affect to body
+// setTimeout(function(){
+//   p.style.display = "inline-block";
+//   typeEffect(p, speed);
+// }, delay);
 
-// type affect to header
-//typeEffect(h1, speed);
-
-
-// type affect to body
-setTimeout(function(){
-  p.style.display = "inline-block";
-  typeEffect(p, speed);
-}, delay);
 
 
